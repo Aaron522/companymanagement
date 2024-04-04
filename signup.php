63efd307486cfa8cd8fb07fd1db3d5b7fde11b0d
Aaron522 <aarn10934@outlook.com>
2024-04-04 04:19:36 +0100

    first commit

---
+++ signup.php
@@ -0,0 +1,43 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "companymanagement";
+if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+    die;
+}
+$firstname = $_POST['fname'];
+$lastname = $_POST['lname'];
+$company = $_POST['company'];
+$formpassword = $_POST['password'];
+$email = $_POST['email'];
+
+
+$conn = new mysqli($host, $user, $password, $db);
+
+if($conn->connect_error) { 
+    die("conn failed: " . $conn->connect_error);
+}
+    
+$stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_rows > 0) { 
+    die("This email already exists");
+}
+$stmt->close();
+$hashpass = password_hash($password, PASSWORD_BCRYPT);
+$stmt = $conn->prepare("INSERT INTO users (firstname, lastname, email, company, position, passwordd) VALUES (?,?,?,?,?,?)");
+$stmt->bind_param("ssssss", $firstname, $lastname, $email, $company, $position, $hashpass);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_rows > 0)
+{
+    echo "Successfully signed up, please now login";
+}
+
+
+
+
